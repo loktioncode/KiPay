e5c20e7dae151adef7468eab5ae2d84cb7df9dbb
Elisha Bere <elishabere4@gmail.com>
2022-07-20 23:24:57 +0200

    add screens

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+} from "react-native";
+import piggyLogo from "./assets/logo.png";
+import { useForm, Controller } from "react-hook-form";
 
 export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={piggyLogo} style={styles.logo} />
+      <Text>TEST APP</Text>
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="lastName"
+      />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+    width: 300,
+    height: 150,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input:{
+    borderColor: "black",
+    width: 280,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+  }
 });
---
+++ config/keys.js
@@ -0,0 +1,11 @@
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyDpukv3TqD1rmJCREvdZdDc8eCdDGLa5PA",
+        authDomain: "react-mobi.firebaseapp.com",
+        projectId: "react-mobi",
+        storageBucket: "react-mobi.appspot.com",
+        messagingSenderId: "178727779112",
+        appId: "1:178727779112:web:849031d83b64c1a1826e1d",
+        measurementId: "G-THSYMJ9CFT"
+      }
+  }
